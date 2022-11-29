20831005c4d29d31168417b1b938b296b9853685
Jeongwon Choi <chlwjddnjs17@naver.com>
2022-11-30 02:05:07 +0900

    FIGHTING!

---
+++ src/Example1.java
@@ -0,0 +1,24 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+/* 
+ * ctrl + shift + L 단축키 전체 목록보기
+ * ctrl + +, - 폰트 크기 증가/감소
+ * ctrl + D 한 줄 삭제
+ * ctrl + alt + down 행 단위 복사
+ * alt + shift + A 멀티컬럼 편집
+ * alt + up, down 행 단위 이동
+ * tab 들여쓰기, shift + tab 내려쓰기		
+ * ctrl + i 자동 들여쓰기
+ * ctrl + / 주석(토글,한번 더 누르면 해제)
+ * ctrl + space 자동완성
+		
+*/
+		System.out.println("Hello,World.");
+		
+		
+	}
+
+}
